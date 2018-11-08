a6267f00c70fe906eaef73d3e094141a0646482b
Carly Lokare <carlylokare@gmail.com>
2018-11-08 15:38:34 -0700

    Added drill

---
+++ BooleanLogicApp/BooleanLogicApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is you age?");
+            string age = Console.ReadLine();
+            int myAge = Convert.ToInt32(age);
+
+            Console.WriteLine("Have you ever had a DUI? Answer True or False");
+            string dui = Console.ReadLine();
+            bool truthyOrFalsey = Convert.ToBoolean(dui);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string tickets = Console.ReadLine();
+            int myTickets = Convert.ToInt32(tickets);
+
+            Console.WriteLine("Qualified?");
+            bool qualified = (myAge > 15 && truthyOrFalsey == false && myTickets < 4);
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ While.DoWhileLoops/While.DoWhileLoops/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While.DoWhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WHILE LOOP
+            Console.WriteLine("Guess a color?");
+            string color = Console.ReadLine();
+            bool isGuessed = color == "blue";
+
+            while (!isGuessed)
+            {
+                switch (color)
+                {
+                    case "red":
+                        Console.WriteLine("You guessed Red. Try again");
+                        Console.WriteLine("Guess a color?");
+                        color = Console.ReadLine();
+                        break;
+                    case "green":
+                        Console.WriteLine("You guessed Green. Ty again");
+                        Console.WriteLine("Guess a color?");
+                        color = Console.ReadLine();
+                        break;
+                    case "purple":
+                        Console.WriteLine("You guessed Purple. Try again");
+                        Console.WriteLine("Guess a color?");
+                        color = Console.ReadLine();
+                        break;
+                    case "blue":
+                        Console.WriteLine("You guessed Blue. That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a color?");
+                        color = Console.ReadLine();
+                        break;
+                }
+            }
+
+            Console.ReadLine();
+
+            //DO WHILE LOOP
+
+            Console.WriteLine("Guess a color?");
+            string colorFav = Console.ReadLine();
+            bool isGuessedColor = colorFav == "blue";
+
+            do
+            {
+                switch (color)
+                {
+                    case "red":
+                        Console.WriteLine("You guessed Red. Try again");
+                        Console.WriteLine("Guess a color?");
+                        colorFav = Console.ReadLine();
+                        break;
+                    case "green":
+                        Console.WriteLine("You guessed Green. Ty again");
+                        Console.WriteLine("Guess a color?");
+                        colorFav = Console.ReadLine();
+                        break;
+                    case "purple":
+                        Console.WriteLine("You guessed Purple. Try again");
+                        Console.WriteLine("Guess a color?");
+                        colorFav = Console.ReadLine();
+                        break;
+                    case "blue":
+                        Console.WriteLine("You guessed Blue. That is correct!");
+                        isGuessedColor = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a color?");
+                        colorFav = Console.ReadLine();
+                        break;
+                }
+            }
+
+            while (!isGuessed);
+
+            Console.ReadLine();
+        }
+    }
+}
