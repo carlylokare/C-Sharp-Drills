31ab8de8ac04a77ebc806dd7e505f0531621c1ee
Carly Lokare <carlylokare@gmail.com>
2018-11-08 15:44:47 -0700

    Added drill

---
+++ ClassMethodAssignment/ClassMethodAssignment/Division.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    class Division
+    {
+        public void Divide2(int number)
+        {
+            Console.WriteLine(number / 2);
+        }
+       
+        public void getValues(out int x, out int y)
+        {
+            Console.WriteLine("Enter the first value");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second value");
+            y = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public static void whichOne(int x)
+        {
+            Console.WriteLine("whichOne(int x)");
+        }
+
+        public static void whichOne(string y)
+        {
+            Console.WriteLine("whichOne(string y)");
+        }
+
+        
+    }
+}
---
+++ ClassMethodAssignment/ClassMethodAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Pick a whole number to divide by 2");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Division example = new Division();
+            example.Divide2(number);
+            Console.ReadKey();
+
+
+            Division example2 = new Division();
+            int a, b;
+            example2.getValues(out a, out b);
+            Console.WriteLine("After method call, value of a: {0}", a);
+            Console.WriteLine("After method call, value of b: {0}", b);
+            Console.ReadKey();
+
+            //Division example3 = new Division();
+            Division.whichOne("test");
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
