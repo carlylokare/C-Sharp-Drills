3601488867e2aebe16c6972755f110d0c170a1c9
Carly Lokare <carlylokare@gmail.com>
2018-11-08 15:45:47 -0700

    Added drill

---
+++ Abstract_Assignment/Abstract_Assignment/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Assignment
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("First Name: " + firstName);
+            Console.WriteLine("Last Name: " + lastName);
+        }
+}
+}
---
+++ Abstract_Assignment/Abstract_Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee human = new Employee();
+            human.firstName = "Sample";
+            human.lastName = "Student";
+            human.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
