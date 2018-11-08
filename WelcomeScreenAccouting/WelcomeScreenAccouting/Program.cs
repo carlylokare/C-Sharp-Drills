14dedfca834ad9c2393281ab23c20a966e795294
Carly Lokare <carlylokare@gmail.com>
2018-11-08 15:31:58 -0700

    Added drill.

---
+++ WelcomeScreenAccouting/WelcomeScreenAccouting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace WelcomeScreenAccouting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting System");
+            Console.ReadLine();
+            Console.WriteLine("Remember, we're accounting for you!");
+            Console.ReadLine();
+        }
+    }
+}
